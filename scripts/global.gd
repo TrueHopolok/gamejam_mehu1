158676a4ffddc353e442f8edf5a3a30b3d699934
Роман Труханов <vadim.truhanov@inbox.lv>
2024-06-16 15:50:40 +0300

    finish

--- scripts/global.gd
+++ scripts/global.gd
@@ -3,10 +3,12 @@ extends Node
 @onready var random_generator = RandomNumberGenerator.new()
 
 var junk_types : Array[PackedScene] = \
-[]
-var fishes_types : Array[PackedScene] = \
-[]
-var unlocked_fish = 0
+[
+	load("res://scenes/junk/junk_wood.tscn"),
+	load("res://scenes/junk/junk_rope.tscn"),
+	load("res://scenes/junk/junk_metal.tscn"),
+]
+var fish_prefab : PackedScene = load("res://scenes/fish/fish_pirahna.tscn")
 var materials_amount : Array[int] = \
 [0, 0, 0, 0]
 var fishes_killed : int = 0
@@ -18,86 +20,55 @@ var extra_durability : int = 0
 var extra_projectile_damage : float = 0
 var extra_raft_health : float = 0
 
-func fish_killed(prize : Array[int]):
+func fish_killed():
 	fishes_alive -= 1
 	if fishes_alive <= 0:
 		waves_completed += 1
-		await get_tree().create_timer(10).timeout
+		await get_tree().create_timer(5).timeout
 		spawn_wave() 
 
 func spawn_junk():
-	var main : Node2D
-	for element in get_tree().get_root().get_children():
-		if element.name == "Main":
-			main = element
-			break
+	var main = get_tree().get_first_node_in_group("Main")
 	var generated_value = random_generator.randf()
 	var i : int = 0
 	if generated_value >= 0.80: i = 2
 	elif generated_value >= 0.45: i = 1
-	var instance = junk_types[max(i, len(junk_types))].instantiate()
+	var instance = junk_types[min(i, len(junk_types)-1)].instantiate()
 	main.add_child(instance)
 	instance.global_position = Vector2(randi_range(330, 360), 158)
 	instance.died.connect(spawn_junk)
 
-func spawn_realboss():
-	pass
-
-func spawn_miniboss():
-	pass
-
 func spawn_wave():
-	var main : Node2D
-	for element in get_tree().get_root().get_children():
-		if element.name == "Main":
-			main = element
-			break
-	if (waves_completed + 1) % 25 == 0:
-		fishes_alive = 1
-		spawn_realboss()
-	elif (waves_completed + 1) % 5 == 0:
-		fishes_alive = 1
-		spawn_miniboss()
-	elif (waves_completed + 1) % 3 == 0 and unlocked_fish < len(fishes_types):
-		fishes_alive = waves_completed
-		var j : int
-		var instance : Node2D
-		for i in fishes_alive:
-			j = random_generator.randi_range(0, len(fishes_types)-1)
-			instance = fishes_types[j].instantiate()
-			main.add_child(instance)
-			instance.global_position.x = random_generator.randi_range(260, 320)
-			instance.global_position.y = random_generator.randi_range(30, 150)
-		instance = fishes_types[unlocked_fish].instantiate()
+	var main = get_tree().get_first_node_in_group("Main")
+	fishes_alive = clampi(1 + waves_completed - int(waves_completed / 5), 1, 20)
+	var j : int
+	var instance : Node2D
+	for i in fishes_alive:
+		instance = fish_prefab.instantiate()
 		main.add_child(instance)
 		instance.global_position.x = random_generator.randi_range(260, 320)
 		instance.global_position.y = random_generator.randi_range(30, 150)
-		fishes_alive += 1
-		unlocked_fish += 1
-	else:
-		fishes_alive = clampi(1 + waves_completed - int(waves_completed / 5), 1, 20)
-		var j : int
-		var instance : Node2D
-		for i in fishes_alive:
-			j = random_generator.randi_range(0, len(fishes_types)-1)
-			instance = fishes_types[j].instantiate()
-			main.add_child(instance)
-			instance.global_position.x = random_generator.randi_range(260, 320)
-			instance.global_position.y = random_generator.randi_range(30, 150)
+		instance.died.connect(fish_killed)
 
-func load_main_menu():
-	# TODO: switch tree main node to main menu node
-	pass
 
 func load_game_screen():
-	# TODO: switch tree root to game screen node
-	# TODO: reset all data
-	await get_tree().create_timer(10).timeout 
+	get_tree().get_root().get_child(1).queue_free()
+	get_tree().get_root().add_child(load("res://scenes/main.tscn").instantiate())
+	materials_amount = [0, 0, 0, 0]
+	fishes_killed = 0
+	fishes_alive = 0
+	waves_completed = 0
+	extra_melee_damage = 0
+	extra_durability = 0
+	extra_projectile_damage = 0
+	extra_raft_health = 0
+	spawn_junk()
+	await get_tree().create_timer(3).timeout 
 	spawn_wave()
 
 func load_game_over():
-	# TODO: pause game (and game ui also)
-	# TODO: open gameover screen menu
+	get_tree().get_root().get_child(1).queue_free()
+	get_tree().get_root().add_child(load("res://scenes/main_menu.tscn").instantiate())
 	pass
 
 func _ready():
---
+++ scripts/main_menu.gd
@@ -0,0 +1,4 @@
+extends Button
+
+func _pressed():
+	Global.load_game_screen()
